055e8c7a84866655fae68b3d3c67b0f9010e45e4
avi78 <aviternal20@gmail.com>
2024-03-25 11:12:40 +0530

    completed

--- paint-my-mood/app/page.tsx
+++ paint-my-mood/app/page.tsx
@@ -1,48 +1,105 @@
 "use client"
 import axios from "axios";
-import Image from "next/image";
 import { ChangeEvent, useEffect, useState } from "react";
+import { emotionConfig } from "./config";
+import { ColorRing } from "react-loader-spinner";
 
 export default function Home() {
-  // state variables
+  const defaultColor = "#cccccc";
   const [rows, setRows] = useState(2);
-  const [input,setInput]= useState("");
+  const [input, setInput] = useState("");
+  const [output, setOutput] = useState<any[]>();
   const [loading, setLoading] = useState(false);
+  const [color, setColor] = useState(defaultColor);
+  const [tagsVisible, setTagsVisible] = useState(false);
 
   useEffect(() => {
     const inputTimeout = setTimeout(() => {
-        runPredictions();
-    },1000);
+      runPredictions();
+    }, 1000);
 
     return () => clearTimeout(inputTimeout);
-  }, [input])
+  }, [input]);
+
+  useEffect(() => {
+    handleColor();
+    setTagsVisible(true);
+  }, [output]);
 
   async function runPredictions() {
     if (input) {
       setLoading(true);
-      // send api call
-      const res = await axios.post('api/emotion',{ input })
-      console.log(res);
-      setLoading(false);
+      setTagsVisible(false);
+      try {
+        const res = await axios.post("/api/emotion", { input });
+        setOutput(res.data.filteredResponse);
+      } catch (error) {
+        console.error("Error fetching predictions:", error);
+      } finally {
+        setLoading(false);
+      }
+    }
+  }
+
+  function handleColor() {
+    if (output && output.length > 0) {
+      const colorKey = output[0].label;
+      const colorHex = emotionConfig[colorKey].colorHex;
+      setColor(colorHex);
     }
   }
 
   function handleInputChange(event: ChangeEvent<HTMLTextAreaElement>): void {
     setInput(event.target.value);
-    // increase the number of rows if required
     const newRows = Math.max(1, Math.ceil(event.target.scrollHeight / 20));
     setRows(newRows);
   }
 
   return (
-    <main className="gap-4 flex min-h-screen flex-col items-center p-24">
-      <h1 className="lg:text-4xl text-2xl font-mono font-semibold tracking-tight">🎨🖌 Paint My Mood</h1>
-      <div className="border border-black p-4 rounded-lg">
-        <textarea rows={rows} onChange={handleInputChange} placeholder="type how you feel . . ." className="resize-none outline-none block w-full text-sm plcaeholder-slate-600 bg-transparent">
-
-        </textarea>
+    <main
+      style={{ backgroundColor: color + "aa" }}
+      className="transition-all delay-500 gap-4 flex min-h-screen flex-col items-center p-24"
+    >
+      <h1 className="lg:text-4xl text-2xl font-mono font-semibold tracking-tight">
+        🎨🖌️ Paint My Mood{" "}
+      </h1>
+      <div className="w-1/2 min-w-80 border-2 border-black p-4 rounded-lg">
+        <textarea
+          rows={rows}
+          onChange={handleInputChange}
+          placeholder="Type how you feel . . ."
+          className="resize-none outline-none block w-full text-sm placeholder-slate-600 bg-transparent"
+        ></textarea>
       </div>
-      <p>{'> ' + input}</p>
+      <p>{"> " + input}</p>
+      <div className="flex flex-wrap items-center justify-center gap-2">
+        {output?.map(({ label, score }) => {
+          return (
+            <span
+              style={{ opacity: tagsVisible ? 1 : 0 }}
+              key={label}
+              className="transition-all cursor-pointer bg-indigo-100 text-indigo-800 text-lg px-4 py-1 rounded-full border border-indigo-400"
+            >
+              {label} {emotionConfig[label].emoji}
+            </span>
+          );
+        })}
+      </div>
+      {loading && renderLoader()}
     </main>
   );
+
+  function renderLoader() {
+    return (
+      <ColorRing
+        visible={true}
+        height="80"
+        width="80"
+        ariaLabel="color-ring-loading"
+        wrapperStyle={{}}
+        wrapperClass="color-ring-wrapper"
+        colors={["#e15b64", "#f47e60", "#f8b26a", "#abbd81", "#849b87"]}
+      />
+    );
+  }
 }
